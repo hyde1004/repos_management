6691c3fdfd9d669a45e5931c05ac4e4bedd8a615
Junggu Lee <junggu.lee@technicolor.com>
2020-06-09 10:58:03 +0900

    update

--- git_sdk_push_garnet.py
+++ git_sdk_push_garnet.py
@@ -22,7 +22,7 @@ repo_names = [
     ('syna-release/toolchain', 'android/syna-release/toolchain'),
     ('syna-release/fw_enc', 'android/syna-release/fw_enc'),
     ('syna-release/ta_enc', 'android/syna-release/ta_enc/root'),
-    ('syna-release/ampsdk', 'android/syna-release/ampsdk/root'),
+#    ('syna-release/ampsdk', 'android/syna-release/ampsdk/root'),
     ('syna-release/osal', 'android/syna-release/osal'),
     ('android/vendor/imagination', 'android/aosp_mirror/platform/vendor/imagination'),
     ('android/vendor/marvell', 'android/aosp_mirror/platform/vendor/marvell'),
@@ -38,16 +38,28 @@ repo_names = [
     ('android/vendor/vsi', 'android/aosp_mirror/platform/vendor/vsi'),
 
     # IP
-    ('syna-release/ampsdk/amp/src/ddl/acodec/codec/codec_mpeg', 'android/syna-release/ampsdk/amp/src/ddl/acodec/codec/codec_mpeg'),
-    ('syna-release/fw_enc/mp3', 'android/syna-release/fw_enc/mp3'),
+    #('syna-release/ampsdk/amp/src/ddl/acodec/codec/codec_mpeg', 'android/syna-release/ampsdk/amp/src/ddl/acodec/codec/codec_mpeg'),
+    #('syna-release/fw_enc/mp3', 'android/syna-release/fw_enc/mp3'),
     ('syna-release/ta_enc/libwatermark.ta', 'android/syna-release/ta_enc/libwatermark.ta'),
+	('android/vendor/synaptics/netflix', 'android/aosp_mirror/platform/vendor/synaptics/netflix'),
+	('syna-release/ampsdk/amp/src/ddl/acodec/codec/codec_wma', 'android/syna-release/ampsdk/amp/src/ddl/acodec/codec/codec_wma'),
+	('syna-release/ta_enc/libwma.ta', 'android/syna-release/ta_enc/libwma.ta'),
+	('android/vendor/synaptics/playready', 'android/aosp_mirror/platform/vendor/synaptics/playready'),
+	('android/vendor/synaptics/widevine', 'android/aosp_mirror/platform/vendor/synaptics/widevine'),
+	('syna-release/ampsdk/drm/widevine', 'android/syna-release/ampsdk/drm/widevine'),
+	('syna-release/drm/pr_syna_ca', 'android/syna-release/drm/pr_syna_ca/root'),
+	('syna-release/drm/pr_syna_ca/4x', 'android/syna-release/drm/pr_syna_ca/4x'),
+	('syna-release/ta_enc/pr40', 'android/syna-release/ta_enc/pr40'),
+	('syna-release/ta_enc/wv15', 'android/syna-release/ta_enc/wv15'),
+#new	('android/vendor/synaptics/amplitude', 'android/aosp_mirror/platform/vendor/synaptics/amplitude'),
+#new	('syna-release/ampsdk/amp/src/ddl/comp_aout/Dolby_DCV', 'android/syna-release/ampsdk/amp/src/ddl/comp_aout/Dolby_DCV'),
 
     # OT
     ('android/device/synaptics/igarnet', 'android/aosp_mirror/device/synaptics/igarnet'),
     ('android/device/synaptics/igarnet-kernel', 'android/aosp_mirror/device/synaptics/igarnet-kernel'),
     ('android/vendor/synaptics/igarnet', 'android/aosp_mirror/platform/vendor/synaptics/igarnet'),
     ('syna-release/cust/igarnet', 'android/syna-release/cust/igarnet'),
-
+	
 ]
 
 LOCAL_BRANCH = 'igarnet/vs680/AndroidQ/20200515/202005111414/SDK'
@@ -56,10 +68,14 @@ LOCAL_BRANCH = 'igarnet/vs680/AndroidQ/20200515/202005111414/SDK'
 TCH_BRANCH = 'tch/synaptics-sdk/igarnet_vs680_AndroidQ_20200515_202005111414_SDK'
 
 TAG = 'igarnet_20200515_SDK_Release'
+DO_DRY_RUN = True
 
 try:
+	dry_run = ' --dry-run' if DO_DRY_RUN else ''
+		
     for (syn_path, tch_repo) in repo_names:
-        print('##### repo name : %s' % (syn_path))
+		index = repo_names.index((syn_path, tch_repo))
+        print('##### repo name : %s [%d/%d]' % (syn_path, index + 1, len(repo_names))
 
         root_pwd = os.getcwd()
         os.chdir(syn_path)
@@ -67,8 +83,8 @@ try:
         # checkout
         cmd = "git checkout " + LOCAL_BRANCH
         print('cmd: %s' % cmd)
-        subprocess.check_call(cmd.split(' ')) # Need to Check result
-        #subprocess.call(cmd.split(' ')) # Don't need to check result 
+        #subprocess.check_call(cmd.split(' ')) # Need to Check result
+        subprocess.call(cmd.split(' ')) # Don't need to check result 
 
         # add remote
         cmd = 'git remote add gitolite git@gitolite.bundang.ap.thmulti.com:/' + tch_repo +'.git'
@@ -77,13 +93,14 @@ try:
         subprocess.call(cmd.split(' ')) # Don't need to check result
 
         # push branch
-        cmd = "git push gitolite " + LOCAL_BRANCH +":" + TCH_BRANCH #+ " --dry-run" 
+        #cmd = "git push gitolite " + LOCAL_BRANCH +":" + TCH_BRANCH #+ " --dry-run" 
+        cmd = "git push gitolite " " :" + TCH_BRANCH + dry_run 
         print('cmd: %s' % cmd)
         #subprocess.check_call(cmd.split(' ')) # Need to Check result
         subprocess.call(cmd.split(' ')) # Don't need to check result 
 
         # push tag
-        cmd = "git push gitolite " + TAG #+ " --dry-run"
+        cmd = "git push gitolite  :" + TAG + dry_run 
         print('cmd: %s' % cmd)
         subprocess.check_call(cmd.split(' ')) # Need to Check result
         #subprocess.call(cmd.split(' ')) # Don't need to check result 
